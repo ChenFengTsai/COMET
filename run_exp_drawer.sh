dc7dcbe7ec08e9a975af9dd3583a3356fe31feb1
ChenFengTsai <rich.tsai1103@gmail.com>
2025-11-25 17:36:18 +0800

    shell fix

--- run_exp_drawer.sh
+++ run_exp_drawer.sh
@@ -4,21 +4,23 @@
 
 BASE_CMD="python dreamer_distill.py"
 CONFIGS="--configs defaults metaworld"
+TASK="metaworld_drawer_open"
+
+# this is for original_cnn
+# LOGDIR_BASE="/storage/ssd1/richtsai1103/vid2act/log/metaworld/mt6/drawer_open/original"
+# TEACHER_ENCODER_MODE="original_cnn" # moe or original_cnn
+# DEVICE="cuda:4"
+# TEACHER_MODEL_PATH="/storage/ssd1/richtsai1103/vid2act/models/mt6/original/teacher_model.pt"
+# VAE_MODEL_PATH="/storage/ssd1/richtsai1103/vid2act/models/mt6/original/vae_model.pt"
+
+# this is for moe
 LOGDIR_BASE="/storage/ssd1/richtsai1103/vid2act/log/metaworld/mt6/drawer_open/moe_multihead"
 TEACHER_ENCODER_MODE="moe" # moe or original_cnn
-TASK="metaworld_drawer_open"
 DEVICE="cuda:1"
 TEACHER_MODEL_PATH="/storage/ssd1/richtsai1103/vid2act/models/mt6/moe_multihead/teacher_model.pt"
 VAE_MODEL_PATH="/storage/ssd1/richtsai1103/vid2act/models/mt6/moe_multihead/vae_model.pt"
 
-# BASE_CMD="python dreamer_distill.py"
-# CONFIGS="--configs defaults metaworld"
-# LOGDIR_BASE="/storage/ssd1/richtsai1103/vid2act/log/metaworld/mt6/drawer_open/original"
-# TEACHER_ENCODER_MODE="original_cnn" # moe or original_cnn
-# TASK="metaworld_drawer_open"
-# DEVICE="cuda:4"
-# TEACHER_MODEL_PATH="/storage/ssd1/richtsai1103/vid2act/models/mt6/original/teacher_model.pt"
-# VAE_MODEL_PATH="/storage/ssd1/richtsai1103/vid2act/models/mt6/original/vae_model.pt"
+
 
 # Fixed random seeds
 SEEDS=(0 123 456 789 2024)
